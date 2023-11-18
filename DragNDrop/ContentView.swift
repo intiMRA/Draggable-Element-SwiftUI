87f6d90eaa397d272fcbc1f997553cf9609e8c73
inti <inti.albuquerque@trademe.co.nz>
2023-11-18 22:51:19 +1300

    allowing drag and drop

--- DragNDrop/ContentView.swift
+++ DragNDrop/ContentView.swift
@@ -33,9 +33,58 @@ struct ContentView: View {
                     fruit.image
                         .resizable()
                         .frame(width: 50, height: 50)
+                        .onDrag({
+                            self.selectedItem = fruit
+                            return MyItemProvider(selectedItem: $selectedItem, draggableItems: $draggableItems, originalItems: draggableItems)
+                        })
+                        .onDrop(of: [.text], delegate: MyDropDelegate(selectedItem: $selectedItem, draggableItems: $draggableItems, destinationItem: fruit))
                 }
             }
+            .animation(.easeIn, value: draggableItems)
         }
         .padding()
+    }
+}
+
+class MyItemProvider: NSItemProvider {
+    @Binding var selectedItem: Fruits?
+    @Binding var draggableItems: [Fruits]
+    let originalItems: [Fruits]
+    
+    init(selectedItem: Binding<Fruits?>, draggableItems: Binding<[Fruits]>, originalItems: [Fruits]) {
+        self._selectedItem = selectedItem
+        self._draggableItems = draggableItems
+        self.originalItems = originalItems
+        super.init()
+    }
+    
+    deinit {
+        if self.selectedItem != nil {
+            self.draggableItems = self.originalItems
+        }
+    }
+}
+
+class MyDropDelegate: DropDelegate {
+    @Binding var selectedItem: Fruits?
+    @Binding var draggableItems: [Fruits]
+    let destinationItem: Fruits
+    
+    init(selectedItem: Binding<Fruits?>, draggableItems: Binding<[Fruits]>, destinationItem: Fruits) {
+        self._selectedItem = selectedItem
+        self._draggableItems = draggableItems
+        self.destinationItem = destinationItem
+    }
+    
+    func performDrop(info: DropInfo) -> Bool {
+        self.selectedItem = nil
+        return false
+    }
+    
+    func dropEntered(info: DropInfo) {
+        if let newIndex = draggableItems.firstIndex(where: { $0 == destinationItem }), let selectedItem {
+            draggableItems.removeAll(where: { $0 == selectedItem })
+            draggableItems.insert(selectedItem, at: newIndex)
+        }
     }
 }
